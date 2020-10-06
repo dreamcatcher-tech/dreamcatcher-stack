dfb953e356e88a6a43d82c51b1fcf98df7220db5
evil_liskov <evil_liskov@protonmail.com>
2020-10-06 20:22:19 +0000

    expand side effect permissions

--- w015-models/src/models/configModel.js
+++ w015-models/src/models/configModel.js
@@ -4,16 +4,29 @@ const schema = {
   title: 'Config',
   description: `Configuration options for the chain`,
   type: 'object',
-  required: ['isPierced', 'isSideEffectCapable', 'isPublicChannelOpen'],
+  required: ['isPierced', 'sideEffects', 'isPublicChannelOpen'],
   additionalProperties: false,
   properties: {
     isPierced: {
       type: 'boolean',
       description: 'If the chain should be reduced, even with no known actions',
     },
-    isSideEffectCapable: {
-      type: 'boolean',
+    sideEffects: {
+      type: 'object',
       description: 'When reducing, will this chain be allowed network access',
+      required: ['networkPatterns', 'asyncTimeoutMs'],
+      properties: {
+        networkPatterns: {
+          type: 'array',
+          description: 'Empty string means no network access',
+          items: { type: 'string', format: 'regex' },
+        },
+        asyncTimeoutMs: {
+          type: 'number',
+          description: '0 means no timeout allowed',
+          minimum: 0,
+        },
+      },
     },
     isPublicChannelOpen: {
       type: 'boolean',
@@ -26,14 +39,17 @@ const schema = {
 const configModel = standardize({
   schema,
   create(opts = {}) {
+    const sideEffects = { networkPatterns: [''], asyncTimeoutMs: 0 }
+    opts = { ...opts, sideEffects: { ...sideEffects, ...opts.sideEffects } }
     const config = {
       isPierced: false,
-      isSideEffectCapable: false,
+      sideEffects,
       isPublicChannelOpen: false,
     }
     return configModel.clone({ ...config, ...opts })
   },
   logicize(instance) {
+    // TODO check that if no async, network is also disabled
     return {}
   },
 })
