5127b855c16fa79f11446283348bdc596124086d
evil_liskov <evil_liskov@protonmail.com>
2020-10-15 22:39:23 +0000

    validators

---
+++ packages/dos/src/commands/validators.js
@@ -0,0 +1,12 @@
+const debug = require('debug')('dos:commands:add')
+const chalk = require('ansi-colors')
+const { prompt } = require('enquirer')
+
+module.exports = async ({ spinner, blockchain }, ...args) => {}
+
+module.exports.help = `
+View, change the validator set of a chain or group of chains.
+Recursively change all validators of the chains children.
+Validators must accept the role before the handover is complete.
+Can be used to force a change if a chain has stalled.
+`
