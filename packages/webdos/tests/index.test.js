9e5f008302992b38bd641b2ffb077503e49b0fdf
evil_liskov <evil_liskov@protonmail.com>
2021-06-14 21:17:39 +0000

    skip tests

--- packages/webdos/tests/index.test.js
+++ packages/webdos/tests/index.test.js
@@ -1,10 +1,10 @@
 import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 
-import Component from 'src/'
+import { Terminal } from 'src/'
 
-describe('Component', () => {
+describe('Terminal', () => {
   let node
 
   beforeEach(() => {
@@ -16,7 +16,7 @@ describe('Component', () => {
   })
 
   it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
+    render(<Terminal />, node, () => {
       expect(node.innerHTML).toContain('Welcome to React components')
     })
   })
