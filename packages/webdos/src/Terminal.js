0fa5a02f5ef907cc63fc547fefedeba63e760475
evil_liskov <evil_liskov@protonmail.com>
2021-06-16 03:04:01 +0000

    error testing

--- packages/webdos/src/Terminal.js
+++ packages/webdos/src/Terminal.js
@@ -111,6 +111,9 @@ const TerminalContainer = (props) => {
         debug('error loading fonts: ', e)
         console.error('error loading fonts: ', e)
       })
+      .catch((e) => {
+        debug(`another error ?`, e)
+      })
   }, [])
   return <div id="xterm-container" {...props}></div>
 }
