8adc68ad26190020b9e9aa490e5f375cbd5f5226
evil_liskov <evil_liskov@protonmail.com>
2021-09-03 08:18:31 +0000

    cleanup for lerna

---
+++ packages/dos/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { visualizer } from 'rollup-plugin-visualizer'
+import path from 'path'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const { dependencies } = require('./package.json')
+dependencies['chai/index.mjs'] = true
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild', // required to transform biginteger in noble-crypto
+    rollupOptions: {
+      plugins: [visualizer({ filename: './dist/vis.html' })],
+      external: Object.keys(dependencies),
+    },
+    lib: {
+      formats: ['es'],
+      entry: path.resolve('./src/index.js'),
+    },
+  },
+})
