805233d34551fa98e0e0e15a080bfa77e13d7571
evil_liskov <evil_liskov@protonmail.com>
2021-09-04 06:50:58 +0000

    consumer works in dev and build

--- packages/consumer/src/main.jsx
+++ packages/consumer/src/main.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { version } from '../package.json'
+import './demo.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import React, { useEffect, Component } from 'react'
+import ReactDOM from 'react-dom'
+import {
+  Blockchain,
+  Terminal,
+  Router,
+  Switch,
+  Route,
+} from '@dreamcatcher-tech/webdos'
+
+import {
+  About,
+  Account,
+  DialogDatum,
+  CollectionList,
+  Datum,
+  Explorer,
+  Nav,
+  OpenDialog,
+  Settings,
+  MapBackground,
+  AppContainer,
+  Geometry,
+} from '@dreamcatcher-tech/webdos'
+import multi from './multi'
+import Debug from 'debug'
+const debug = Debug('client:tests:App')
+Debug.enable('*:widgets:* *Route *Switch')
+
+const Map = () => {
+  return (
+    <MapBackground>
+      <h1>child drawn on top of map</h1>
+    </MapBackground>
+  )
+}
+
+const Timesheets = () => {
+  return (
+    <div style={{ display: 'flex', flexFlow: 'column', flex: 1 }}>
+      <h4>Demo version: {version}</h4>
+      <Blockchain dev={multi}>
+        <Terminal style={{ height: '280px', background: 'black' }} />
+        <Router>
+          <Switch>
+            <Route covenant="multi">
+              <AppContainer>
+                <Nav />
+                <MapBackground>
+                  <Switch>
+                    <Route path="/timesheets" component={<CollectionList />}>
+                      <Route path="/custNo-*" component={<DialogDatum />} />
+                    </Route>
+                    <Route path="/personnel" component={<CollectionList />}>
+                      <Route path="/custNo-*" component={<DialogDatum />} />
+                    </Route>
+                    <Route path="/sites" component={<Geometry />} />
+                    <Route path="/payments" component={<CollectionList />}>
+                      <Route path="/payNo-*" component={<DialogDatum />} />
+                    </Route>
+                    <Route path="/about" component={<About />} />
+                    <Route path="/settings" component={<Settings />} />
+                    <Route path="/account" component={<Account />} />
+                  </Switch>
+                </MapBackground>
+              </AppContainer>
+            </Route>
+            <Route component={<Explorer />} />
+          </Switch>
+        </Router>
+      </Blockchain>
+    </div>
+  )
+}
+export default class Demo extends Component {
+  render() {
+    return (
+      <div style={{ display: 'flex', flexFlow: 'column', flex: 1 }}>
+        <h4>Demo version: {version}</h4>
+        <Blockchain dev={multi}>
+          <Terminal style={{ height: '280px', background: 'black' }} />
+          <Router>
+            <Switch>
+              <Route covenant="multi">
+                <AppContainer>
+                  <Nav />
+                  <MapBackground>
+                    <Switch>
+                      <Route path="/customers" component={<CollectionList />} />
+                      <Route path="/services" component={<Geometry />} />
+                    </Switch>
+                  </MapBackground>
+                  <Route path="/custNo-*" component={<DialogDatum />} />
+                </AppContainer>
+              </Route>
+              <Route component={<Explorer />} />
+            </Switch>
+          </Router>
+        </Blockchain>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Demo />, document.querySelector('#root'))
--- packages/consumer/vite.config.js
+++ packages/consumer/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-});
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: true,
+    rollupOptions: {
+      plugins: [visualizer({ filename: './dist/vis.html' })],
+    },
+  },
+})
