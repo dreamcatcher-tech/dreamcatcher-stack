fd0cfd9c73624cea995908843d9323a93371fd22
evil_liskov <evil_liskov@protonmail.com>
2021-09-25 20:34:24 +0000

    producers complete

--- pkg/interblock/src/w016-producers/src/blockProducer.js
+++ pkg/interblock/src/w016-producers/src/blockProducer.js
@@ -1,43 +1,37 @@
 import { assert } from 'chai/index.mjs'
+import { dmzModel, blockModel } from '../../w015-models'
 import {
-  dmzModel,
-  provenanceModel,
-  blockModel,
-  integrityModel,
-  ciSigner,
-} from '../../w015-models'
+  generateNextProvenance,
+  generateGenesisProvenance,
+  addSignature,
+  generatePierceProvenance,
+} from './provenanceProducer'
 
-const generateNext = async (dmz, block, asyncSigner = ciSigner) => {
-  // TODO check the dmz follows from the current one ?
-  // TODO handle no change to dmz but change in lineage
-  // TODO put checks in that blocks without new transmissions cannot be created
-  // basically all lineage rules should be checked here
-  assert(dmzModel.isModel(dmz))
-  assert(blockModel.isModel(block))
-  assert(!dmz.equals(block.getDmz()), 'block dmz has not changed')
-  assert(typeof asyncSigner === 'function') // TODO move into blockModel
-
-  const isNewChannels = dmz.network.isNewChannels(block.network)
-  const extraLineage = {}
-  const isGenesis = block.provenance.height === 0
-  if (isNewChannels && !isGenesis) {
-    // TODO go back to the last validator change
-    const template = integrityModel.create()
-    const hash = block.provenance.getAddress().getChainId()
-    const genesis = integrityModel.clone({ ...template, hash })
-    extraLineage[0] = genesis
-  }
-
-  const provenance = await provenanceModel.create(
-    dmz,
-    block.provenance,
-    extraLineage,
-    asyncSigner
-  )
-  const nextBlock = blockModel.clone({ ...dmz, provenance })
-
-  assert(block.isNext(nextBlock))
-  return nextBlock
+const assemble = (unsignedBlock, signature) => {
+  const provenance = addSignature(unsignedBlock.provenance, signature)
+  return blockModel.clone({ ...unsignedBlock, provenance })
 }
-
-export { generateNext }
+const generateUnsigned = (nextDmz, block) => {
+  assert(dmzModel.isModel(nextDmz))
+  assert(blockModel.isModel(block))
+  const provenance = generateNextProvenance(nextDmz, block)
+  return blockModel.clone({ ...nextDmz, provenance })
+}
+const generateGenesisBlock = (dmz, forkedLineages = {}) => {
+  dmz = dmz || dmzModel.create()
+  assert(dmzModel.isModel(dmz))
+  const provenance = generateGenesisProvenance(dmz, forkedLineages)
+  const block = blockModel.clone({ ...dmz, provenance })
+  return block
+}
+const generatePierceBlock = (pierceDmz, block) => {
+  const ioChannel = block.network['.@@io']
+  if (ioChannel) {
+    assert(ioChannel.heavy.provenance)
+    const parentProvenance = ioChannel.heavy.provenance
+    const provenance = generatePierceProvenance(pierceDmz, parentProvenance)
+    return blockModel.clone({ ...pierceDmz, provenance })
+  }
+  return generateGenesisBlock(pierceDmz)
+}
+export { generateGenesisBlock, assemble, generateUnsigned, generatePierceBlock }
---
+++ pkg/interblock/src/w016-producers/src/provenanceProducer.js
@@ -0,0 +1,106 @@
+// produces a prototype of provenance - everything except the actual signature
+// can only be used on blocks 1 or higher
+// must supply parentProvenance
+import { assert } from 'chai/index.mjs'
+import {
+  dmzModel,
+  provenanceModel,
+  integrityModel,
+  addressModel,
+  blockModel,
+  signatureModel,
+} from '../../w015-models'
+
+const generateNextProvenance = (nextDmz, block) => {
+  assert(dmzModel.isModel(nextDmz))
+  assert(blockModel.isModel(block))
+  assert(!nextDmz.equals(block.getDmz()), 'block dmz has not changed')
+  // TODO check the dmz follows from the current one ?
+  // TODO handle no change to dmz but change in lineage
+  // TODO put checks in that blocks without new transmissions cannot be created
+  // basically all lineage rules should be checked here
+
+  const { provenance } = block
+  const isNewChannels = nextDmz.network.isNewChannels(block.network)
+  const lineage = {}
+  const isGenesis = provenance.height === 0
+  if (isNewChannels && !isGenesis) {
+    // TODO go back to the last validator change
+    const hash = provenance.getAddress().getChainId()
+    const genesis = integrityModel.create(hash)
+    lineage[0] = genesis
+  }
+  const dmzIntegrity = integrityModel.create(nextDmz.getHash())
+  const parentIntegrity = provenance.reflectIntegrity()
+  lineage[provenance.height] = parentIntegrity
+  const address = provenance.getAddress()
+  const height = provenance.height + 1
+  const nextProvenance = {
+    dmzIntegrity,
+    height,
+    address,
+    lineage,
+  }
+  const integrity = integrityModel.create(nextProvenance)
+  const signatures = []
+  const unsigned = provenanceModel.clone({
+    ...nextProvenance,
+    integrity,
+    signatures,
+  })
+  return unsigned
+}
+const addSignature = (provenance, signature) => {
+  assert(provenanceModel.isModel(provenance))
+  assert(signatureModel.isModel(signature))
+  assert(!provenance.signatures.length, `temporarily single sign only`)
+  return provenanceModel.clone({ ...provenance, signatures: [signature] })
+}
+const generatePierceProvenance = (pierceDmz, parentProvenance) => {
+  assert(dmzModel.isModel(pierceDmz))
+  assert(provenanceModel.isModel(parentProvenance))
+
+  // TODO assert validator is only the PIERCE key, and public key matches
+  // signs with a predetermined key, since no point invoking crypto on pierce
+
+  const dmzIntegrity = integrityModel.create(pierceDmz.getHash())
+  const parentIntegrity = parentProvenance.reflectIntegrity()
+  const lineage = { [parentProvenance.height]: parentIntegrity }
+  const address = parentProvenance.getAddress()
+  const height = parentProvenance.height + 1
+  const nextProvenance = {
+    dmzIntegrity,
+    height,
+    address,
+    lineage,
+  }
+  const integrity = integrityModel.create(nextProvenance)
+  const signatures = []
+  return provenanceModel.clone({
+    ...nextProvenance,
+    integrity,
+    signatures,
+  })
+}
+const generateGenesisProvenance = (dmz, forkedLineage = {}) => {
+  dmz = dmz || dmzModel.create()
+  assert(dmzModel.isModel(dmz))
+  const dmzIntegrity = integrityModel.create(dmz.getHash())
+  const provenance = {
+    dmzIntegrity,
+    height: 0,
+    address: addressModel.create('GENESIS'),
+    lineage: forkedLineage,
+  }
+  const integrity = integrityModel.create(provenance)
+  const signatures = []
+
+  return provenanceModel.clone({ ...provenance, integrity, signatures })
+}
+
+export {
+  generateNextProvenance,
+  generateGenesisProvenance,
+  generatePierceProvenance,
+  addSignature,
+}
