7bb471a4cc871f2d71f69456772a2d081e24ac11
evil_liskov <evil_liskov@protonmail.com>
2022-01-08 04:12:29 +0000

    app deploy benchmarks

--- pkg/interblock/src/w402-benchmarks/bench.js
+++ pkg/interblock/src/w402-benchmarks/bench.js
@@ -1,6 +1,6 @@
 import Benchmark from 'benchmark'
 import { assert } from 'chai'
-import { effectorFactory } from '../..'
+import { effectorFactory, apps } from '../..'
 import Debug from 'debug'
 const debug = Debug('interblock:benchmarks')
 const suite = new Benchmark.Suite()
@@ -22,12 +22,29 @@ const hotPing = async () => {
   assert.deepEqual(reply, payload)
 }
 
+const publish = async () => {
+  const shell = await effectorFactory('crm')
+  const { crm } = apps
+  const { dpkgPath } = await shell.publish('dpkgCrm', crm.installer)
+  return shell
+}
+
+const install = async (shell) => {
+  const dpkgPath = 'dpkgCrm'
+  await shell.install(dpkgPath, 'crm')
+}
+
+const addCustomer = async (shell) => {
+  const crmActions = await shell.actions('/crm/customers')
+  await crmActions.add({ formData: { custNo: 100, name: 'test name 1' } })
+  //   await crmActions.add({ formData: { custNo: 101, name: 'test name 2' } })
+}
+
 suite
   .add('boot', {
     defer: true,
     fn: async (deferred) => {
       const shell = await effectorFactory()
-      await new Promise((r) => setTimeout(r, 1000))
       await shell.metro.settle()
       deferred.resolve()
     },
@@ -43,6 +60,30 @@ suite
     defer: true,
     fn: async (deferred) => {
       await hotPing()
+      deferred.resolve()
+    },
+  })
+  .add('publish', {
+    defer: true,
+    fn: async (deferred) => {
+      await publish()
+      deferred.resolve()
+    },
+  })
+  .add('install', {
+    defer: true,
+    fn: async (deferred) => {
+      const shell = await publish()
+      await install(shell)
+      deferred.resolve()
+    },
+  })
+  .add('add customer', {
+    defer: true,
+    fn: async (deferred) => {
+      const shell = await publish()
+      await install(shell)
+      await addCustomer(shell)
       deferred.resolve()
     },
   })
