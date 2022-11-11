76a82b77ff8a6ae480860a027c1f540006fc9228
inverted-capital <evil_liskov@protonmail.com>
2022-11-11 20:22:26 +1300

    sourcemap true

--- pkg/webdos/.storybook/main.mjs
+++ pkg/webdos/.storybook/main.mjs
@@ -25,25 +25,12 @@ export default {
       build: {
         ...config.build,
         target: 'es2020',
-        sourcemap: false,
+        sourcemap: true,
         minify: false,
         rollupOptions: {
           output: {
             manualChunks: (id) => {
               // makes build-storybook work again
-              // if (id.includes('pkg/webdos/src/')) {
-              //   return 'webdos'
-              // }
-              // if (id.includes('pkg/interblock/src/')) {
-              //   return 'interblock'
-              // }
-              // if (id.includes('@mui')) {
-              //   console.log(id)
-              //   return 'mui'
-              // }
-              // if (id.includes('faker')) {
-              //   return 'faker'
-              // }
               return 'vendor'
             },
           },
