5a63b1f31153f7e2ad213ee51386eb344ea358ba
inverted-capital <evil_liskov@protonmail.com>
2022-11-16 09:17:09 +1300

    upgrade. leaflet icons working in chromatic.

--- pkg/webdos/src/components/leaflet.js
+++ pkg/webdos/src/components/leaflet.js
@@ -1,21 +1,10 @@
 import 'leaflet/dist/leaflet.css'
-import * as Leaflet from 'leaflet'
-const L = { ...Leaflet }
+import Leaflet from 'leaflet'
 import '@geoman-io/leaflet-geoman-free'
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css'
-
-L.PM.setOptIn(true)
-
-// import 'leaflet-draw/dist/leaflet.draw.css'
-// import 'leaflet-draw/dist/leaflet.draw-src'
-
-// import 'leaflet-providers'
-
-// import 'leaflet.markercluster/dist/MarkerCluster.css'
-// import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
-// import 'leaflet.markercluster'
-
 import 'beautifymarker/leaflet-beautify-marker-icon.css'
 import 'beautifymarker'
-console.log('L', L)
-export default L
+
+Leaflet.PM.setOptIn(true)
+
+export default Leaflet
