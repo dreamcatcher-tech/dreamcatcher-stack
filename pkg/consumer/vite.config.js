6dc14a7aefa5854c6e45eb5380d0015b38feef2c
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2023-02-09 03:57:56 +0000

    consumer update

--- pkg/consumer/vite.config.js
+++ pkg/consumer/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { visualizer } from 'rollup-plugin-visualizer'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    sourcemap: true,
-    rollupOptions: {
-      plugins: [visualizer({ filename: './dist/vis.html' })],
-    },
-  },
+  plugins: [react()],
 })
