2250f027fd82900d4ea76c41005d8fa1fce106ac
inverted_capital <evil_liskov@protonmail.com>
2023-07-21 13:05:12 +1200

    Wrap App in Engine

--- pkg/dreamcatcher/src/main.jsx
+++ pkg/dreamcatcher/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { EngineHOC } from '@dreamcatcher-tech/webdos'
+
+export const AppEngine = EngineHOC(App, 'iplog')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AppEngine />
+  </React.StrictMode>
 )
