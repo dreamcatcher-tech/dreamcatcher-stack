9ed55d3a6896ecdd300ed9e6408fd67399f0bddf
inverted_capital <evil_liskov@protonmail.com>
2023-07-22 12:14:26 +1200

    stability.ai integration

--- pkg/dreamcatcher/src/stories/Stability.jsx
+++ pkg/dreamcatcher/src/stories/Stability.jsx
@@ -72,6 +72,8 @@ export const Stability = ({ onImage }) => {
     form.append('prompt', prompt)
     form.append('preset', style)
     console.log('form', [...form.entries()])
+    const apiKey = import.meta.env.VITE_CLIPDROP_API_KEY
+    console.log('apiKey', apiKey)
     const response = await fetch(CLIPDROP_URL, {
       method: 'POST',
       headers: { 'x-api-key': import.meta.env.VITE_CLIPDROP_API_KEY },
