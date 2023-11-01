f8ef04d2478c9207d803961588ce8bd9968f582c
inverted_capital <evil_liskov@protonmail.com>
2023-11-01 14:40:51 +0000

    useAsync throws

--- pkg/interblock/src/w010-hooks/test/useAsync.test.js
+++ pkg/interblock/src/w010-hooks/test/useAsync.test.js
@@ -4,7 +4,7 @@ import { Reply, RequestId, AsyncTrail, RxReply } from '../../w008-ipld'
 import assert from 'assert-fast'
 
 describe('useAsync', () => {
-  test.only('basic', async () => {
+  test('basic', async () => {
     const effect = async () => {
       return await new Promise((r) => setTimeout(r, 100, 'result'))
     }
@@ -31,7 +31,44 @@ describe('useAsync', () => {
     trail = await wrapReduce(trail, reducer)
 
     expect(trail.isPending()).toBeFalsy()
-    let [set] = trail.txs
   })
-  it.todo('throws on fn throw')
+  it('throws on fn throw', async () => {
+    const error = new Error('throws')
+    const effect = async () => {
+      return await new Promise((_, r) => setTimeout(r, 100, error))
+    }
+    const reducer = async () => {
+      try {
+        await useAsync(effect)
+      } catch (caught) {
+        expect(caught.message).toBe(error.message)
+        return { result: 'caught' }
+      }
+    }
+    let trail = AsyncTrail.createCI()
+    trail = await wrapReduce(trail, reducer)
+    let [io] = trail.txs
+    expect(io.request.type).toBe('@@ASYNC')
+    const fn = popAsyncWhisper(io.request)
+    expect(fn).toEqual(effect)
+    let result
+    try {
+      await fn()
+    } catch (error) {
+      result = error
+    }
+    expect(result).toBeInstanceOf(Error)
+
+    let requestId = RequestId.createCI()
+    io = io.setId(requestId)
+    trail = trail.updateTxs([io])
+
+    const reply = Reply.createError(result)
+    let rxReply = RxReply.create(reply, requestId)
+    trail = trail.settleTx(rxReply)
+    trail = await wrapReduce(trail, reducer)
+
+    expect(trail.isPending()).toBeFalsy()
+    expect(trail.getReply().payload).toEqual({ result: 'caught' })
+  })
 })
