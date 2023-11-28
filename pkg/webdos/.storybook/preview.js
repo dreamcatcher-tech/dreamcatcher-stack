6e7b59c90d88423736a99247480517c29ee34737
inverted_capital <evil_liskov@protonmail.com>
2023-11-29 11:28:17 +1300

    remove storybook console

--- pkg/webdos/.storybook/preview.js
+++ pkg/webdos/.storybook/preview.js
@@ -1,7 +1,4 @@
-import { withConsole } from '@storybook/addon-console'
-
 export default {
-  decorators: [(storyFn, context) => withConsole()(storyFn)(context)],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
